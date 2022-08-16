1b4d0e71eee0a04c0f86d641e3e9949b4435837b
straiker321 <95499138+straiker321@users.noreply.github.com>
2022-08-16 00:41:20 -0500

    hola

--- app/src/main/java/com/example/cineplex_version_final/ui/carrito/Combos_activity.kt
+++ app/src/main/java/com/example/cineplex_version_final/ui/carrito/Combos_activity.kt
@@ -3,6 +3,7 @@ package com.example.cineplex_version_final.ui.carrito
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cineplex_prueba.ui.adapter.OnClickListener
@@ -18,16 +19,22 @@ class Combos_activity : AppCompatActivity() ,OnClickListener{
         super.onCreate(savedInstanceState)
         binding = ActivityCombosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
-        binding.rvCombo.layoutManager = LinearLayoutManager(this)
+        binding.rvCombo.layoutManager = GridLayoutManager(this,2)
         binding.rvCombo.adapter = CombosAdapter(ComboProvider.combolist,this)
     }
 
     override fun onClickItemListener(position: Int) {
         val intent = Intent(this, FDP_activity::class.java)
         startActivity(intent)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_dulceria, menu)
+        return true
     }
 }
